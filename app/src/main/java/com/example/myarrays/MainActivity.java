b7edc644a89a4aa711e6c55478fc8afa1e483c04
Hohoangphu <113907569+Hohoangphu@users.noreply.github.com>
2022-09-19 19:54:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myarrays/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.myarrays;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et_day;
+    TextView tv_kq;
+    Button taoday, xuatxuoi, xuatnguoc, minmax;
+    int myArray[] = new int[10];
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et_day=(EditText)findViewById(R.id.editText);
+        tv_kq=(TextView)findViewById(R.id.textView2); //textView1 ta sửa thuộc tính Text trong giao diện là "Dãy ngẫu nhiên"
+        taoday=(Button)findViewById(R.id.button1);
+        xuatxuoi=(Button)findViewById(R.id.button2);
+        xuatnguoc=(Button)findViewById(R.id.button3);
+        minmax=(Button)findViewById(R.id.button4);
+
+
+        taoday.setOnClickListener(new View.OnClickListener()
+        {
+            public void ArraytoEditText()
+            {
+                String chuoi="";
+                for(int i=0;i<10; i++)
+                {
+                    chuoi +=myArray[i]+ " ";
+                }
+                et_day.setText(chuoi);
+            }
+
+            public void onClick(View v)
+            {
+                Random rd = new Random();
+                String chuoi="";
+                for(int i=0;i<10;i++)
+                {
+                    myArray[i]=rd.nextInt(11); //từ 0 đến 9
+                    //myArray[i]=rd.nextInt(50-10+1)+10; //từ 10->50 // (b-a+1)+a
+                }
+                ArraytoEditText();
+            }
+        });
+
+        xuatxuoi.setOnClickListener(new View.OnClickListener()
+        {
+            public void ArraytoEditText()
+            {
+                String chuoi="";
+                for(int i=0;i<10; i++)
+                {
+                    chuoi +=myArray[i]+ " ";
+                }
+                et_day.setText(chuoi);
+            }
+
+            public void onClick(View v)
+            {
+                ArraytoEditText();
+            }
+        });
+
+        xuatnguoc.setOnClickListener(new View.OnClickListener()
+        {
+
+            public void onClick(View v)
+            {
+                String chuoi="";
+                for(int i=9;i>=0;i--)
+                {
+                    chuoi+= myArray[i]+ " ";
+                }
+                et_day.setText(chuoi);
+            }
+        });
+
+        minmax.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                int min=myArray[0];
+                int max=myArray[0];
+                for(int i=0;i<10;i++)
+                {
+                    if(myArray[i]<min) min=myArray[i];
+                    if(myArray[i]>max) max=myArray[i];
+                }
+                tv_kq.setText("Min: "+min + " max: "+max);
+            }
+        });
+    }
+}
